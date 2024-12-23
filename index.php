7b68fe5010863e9205e89cd30b555f8dbf8c801f
rayx81194 <mani.phartiyal@gmail.com>
2024-12-24 00:41:40 +0530

    tailwind css integration

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todoki - Home</title>
+    <link href="./src/output.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <style>
+        *{
+            font-family: "Space Grotesk", serif;
+        }
+    </style>
 </head>
 <body>
     <!-- Navbar -->
--- signup.php
+++ signup.php
@@ -25,18 +25,33 @@ if  ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todoki - Sign Up</title>
+    <link href="./src/output.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <style>
+        *{
+            font-family: "Space Grotesk", serif;
+        }
+    </style>
 </head>
-<body>
-    <h1>Sign Up</h1>
-    <form action="signup.php" method="post">
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>        
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
-        <button type="submit">Sign Up</button>
-    </form>
+<body class="bg-[#181717] text-white">
+    <!-- Navbar -->
+    <?php include("components/navbar.php"); ?>
+
+    <!-- Main Content -->
+    <div class="container w-1/2 bg-">
+        <form action="signup.php" method="post">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>        
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            <button type="submit">Sign Up</button>
+        </form>
+    </div>
+
+    <!-- Footer -->
+    <?php include("components/footer.php"); ?>
 </body>
 </html>
-<?php
-include("components/footer.php");
-?>
+
