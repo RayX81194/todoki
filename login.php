65bdbfa09f9196db1eddbec55c1adde6c4ea1af7
rayx81194 <mani.phartiyal@gmail.com>
2024-12-29 22:10:13 +0530

    change error handling login

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php
     include("helpers/_dbconnect.php");
 
+    $err= false;
+
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         if(isset($_POST["email"]) && isset($_POST["password"] )){
             $email = $_POST["email"];
@@ -22,7 +24,7 @@
                 }
 
             } else {
-                $err = "Invalid Credentials";
+                $err = "Your Email and Password are incorrect!";
             }
         }
     }
@@ -35,7 +37,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todoki - Login</title>
     <link rel="icon" type="image/x-icon" href="assets/logo.svg">
-    <link href="./assets/css/login.css?v=1" rel="stylesheet">
+    <link href="./assets/css/login.css?v=2" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -48,6 +50,15 @@
     <div class="container">
         <section class="login-section">
             <h1 class="welcome">Welcome Back!</h1>
+            <?php 
+            if($err){
+                echo "
+                <div class='err'>
+                    <img src='assets/Info.svg'>
+                    <p>$err</p>
+                </div>";
+            }
+            ?>
             <form action="login.php" method="post">
                 <input type="email" id="email" name="email" placeholder="Email" required>
                 <input type="password" id="password" name="password" placeholder="Password" required>
