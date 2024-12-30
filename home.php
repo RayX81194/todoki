ecb20ddabc6cc917cd78bc3b07ede68276f63b52
rayx81194 <mani.phartiyal@gmail.com>
2024-12-30 20:28:20 +0530

    edit homepage

--- home.php
+++ home.php
@@ -12,7 +12,7 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todoki - Home</title>
     <link rel="icon" type="image/x-icon" href="assets/logo.svg">
-    <link href="./assets/css/index.css" rel="stylesheet">
+    <link href="./assets/css/home.css?v=1" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -28,15 +28,23 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
 
     <!-- Main Content -->
     <section>
-        <div class="content">
-            <h2>Welcome to Todoki</h2>
-            <p>Explore our features and enjoy your stay.</p>
-            <button type="button">Learn More</button>
+        <div class="slide-show">
+            <div class="slide">
+                <img src="assets/rl.jpg" alt="Slide 1">
+                <div class="genre">
+                    <span>Multiplayer</span>
+                    <span>Soccer</span>
+                    <span>Racing</span>
+                </div>
+                <h1>Rocket League</h1>
+                <p>The most popular game in the world</p>
+            </div>
         </div>
-        
     </section>
 
     <!-- Footer -->
     <?php include("components/footer.php"); ?>
+
+    <script src="./assets/js/script.js"></script>
 </body>
 </html>
